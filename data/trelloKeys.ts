b8ba47937cae1f552e62138e2d2d52e78f22c37d
oscarcabezuelo <cabezuelo@oscala.es>
2024-08-09 13:45:48 +0200

    remove trello keys

--- data/trelloKeys.ts
+++
@@ -1,6 +0,0 @@
-export const BOARD_ID = "66ace05a85708be4d545cc2d";
-export const API_KEY = "1ed1701d67342a3b7b94eda70273cd1b";
-export const TOKEN =
-  "ATTAcbd605476f031cf57d5fda39e79045bbc2016c02341ee6e2ea92b9c8c39fa786B6DC33E5";
-export const PENDING_LIST_ID = "66ace0603bc606b5b05c86bc";
-export const DONE_LIST_ID = "66ace062cf73e952f8ecee2c";
